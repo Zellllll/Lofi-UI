7f88364f98736aed388b58e8d4270d87c243a54b
Zell <58902997+Zellllll@users.noreply.github.com>
2021-02-28 13:12:27 +0100

    Update Window.lua

--- Lofi/Window.lua
+++ Lofi/Window.lua
@@ -5,13 +5,13 @@ local UserInputService = game:GetService("UserInputService")
 function Window:New()
     local Window = {}
 
-    Window.Object = Lofi.require("Lofi\\Base Objects\\Window.lua")()
+    Window.Object = Lofi.require("Lofi/Base Objects/Window.lua")()
     Window.Object.Parent = game.CoreGui
 
     Window.Tiles = 1
 
     function Window:Tile(icon, label)
-        local Tile = Lofi.require("Lofi\\Tile.lua"):New(icon, label, Window)
+        local Tile = Lofi.require("Lofi/Tile.lua"):New(icon, label, Window)
     
         return Tile
     end
